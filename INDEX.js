a50663bf0e3487276e4956bdcbd983e5b01ae81d
Jah821 <91166820+Jah821@users.noreply.github.com>
2021-09-29 18:36:08 +0000

    Jahsanni Williams answered number 2

--- INDEX.js
+++ INDEX.js
@@ -5,4 +5,14 @@ function OneHundredtoFive(){
       console.log(i)
   }
 }
-OneHundredtoFive()
+// HundredtoFive()
+
+function absoluteValue(number){
+  let answer = Math.abs(number)
+  if (answer === 0){
+    return null
+} else {
+  return answer
+}
+}
+// console.log(absoluteValue(-20))
